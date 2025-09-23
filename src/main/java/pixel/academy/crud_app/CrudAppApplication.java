6af1968e8ece308c920dd426dcb7c3a158449c3a
Sorin Gore <soringoredev@gmail.com>
2025-09-23 17:41:35 +0300

    Inject StudentDAO

--- src/main/java/pixel/academy/crud_app/CrudAppApplication.java
+++ src/main/java/pixel/academy/crud_app/CrudAppApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import pixel.academy.crud_app.dao.StudentDAO;
 
 @SpringBootApplication
 public class CrudAppApplication {
@@ -13,10 +14,13 @@ public class CrudAppApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello, world!");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
 	}
 
 }
