2274c42c4f2ef8e0be52afa8b94060a4d857db50
Sorin Gore <soringoredev@gmail.com>
2025-09-27 16:07:31 +0300

    Create List for StudentDAO

--- src/main/java/pixel/academy/crud_app/dao/StudentDAO.java
+++ src/main/java/pixel/academy/crud_app/dao/StudentDAO.java
@@ -2,8 +2,14 @@ package pixel.academy.crud_app.dao;
 
 import pixel.academy.crud_app.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
+
+
 }
